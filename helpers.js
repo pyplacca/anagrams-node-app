20ce416ecaa0a9314e6af9fce1e97a6c7d56de6f
pyPlacca <dhegreatprosperity10@gmail.com>
2020-04-15 21:39:09 +0000

    created file for slightly generic helper functions

--- helpers.js
+++ helpers.js
@@ -1,106 +1,55 @@
-class DefaultDict {
+const appname = 'Anagramator'
 
-  constructor(defaultInit) {
-    return new Proxy({}, {
-        get: (target, name) => name in target ? 
-            target[name] : ( target[name] = typeof defaultInit === 'function' ? 
-                new defaultInit().valueOf() : defaultInit)
-    })
-  }
-}
+// apply ui color 
+function applyColor(color) {
+    const root = document.documentElement
 
-class Trie {
-
-    constructor () {
-        this.children = new DefaultDict ( Trie )
-        this.end = false
-    }
-    
-    find (str) {
-        return !str ? this.end : 
-            !(str[0] in this.children) ? false : 
-                this.children[str[0]].find(str.substr(1))
-    }
-    
-    getAll ( check_str, found='', result=[] ) {
-        if ( this.end ) {
-            found.length > 1 ? result.push(found) :
-            null
-        }
-        for ( const child in this.children ) {
-            if ( check_str.includes( child ) ) {
-                const index = check_str.indexOf( child )
-                
-                this.children[child].getAll(
-                    check_str.substr( 0, index ) + check_str.substr( index + 1 ),
-                    found + child,
-                    result
-                )
-            }
-        }
-        return result
-    }
-    
-    insert ( str ) {
-        !str ? this.end = true : 
-            this.children[str[0]].insert( str.slice(1) )
-    }
-
-    insertMany(...strings) {
-        for (str of strings) {
-            this.insert(str)
-        }
-    }
-}
-
-function groupByLength(arr) {
-    // returns an object of items 
-    let output = new Object()
-
-    for (let item of arr) {
-        n = item.length
-        if (!(n in output)) {
-            output[n] = new Array()
-        }
-        output[n].push(item)
-    }
-    return output
-}
-
-function sorted(arr, opts={ key:undefined, reverse:false }) {
-    arr = new Array(...arr).sort(opts.key)
-    if (opts.reverse) {
-        arr.reverse()
-    }
-    return arr
-}
-
-function reversed(arr) {
-    arr = new Array(...arr)
-    arr.reverse()
-    return arr
-}
-
-/*
-function calcLightness(r, g, b, alpha) {
-    alpha = alpha > 1 ? alpha / 100 : alpha
-
-    let [red, green, blue] = [r, g, b].map(v =>
-        (255 - v) * alpha + v
+    Object.entries(color).forEach(
+        arg => root.style.setProperty(
+            '--color-' + arg[0], arg[1]
+        )
     )
-    console.log(red, green, blue)
-    if (red > 255 || green > 255 || blue > 255) {
-        return r, g, b
-    }
-    return [red, green, blue].map(c => parseInt(c))
 }
-*/
 
+// change ui colors based on color option selected
+function changeAppUIColor(event) {
+    const option = event.target
+
+    if (option.className === 'color-option') {
+
+        const color = ui_colors[
+            option.attributes.getNamedItem('id').value
+        ]
+        applyColor(color)
+    }
+}
+
+function makeCollapsable(tag) {
+    tag.addEventListener(
+        'click', (event) => {
+            elem = event.target
+            elem.classList.toggle('hidden-active')
+            elem.nextElementSibling.classList.toggle('hidden')
+        }
+    )
+}
+
+function showWordCount(elem, arr) {
+    word_count = arr.length;
+    elem.querySelector('#word-count').innerText = `\
+        ${word_count} word${word_count > 1 ? 's' : ''}\
+    `
+}
+
+function updateTitle(...strs) {
+    const title = document.querySelector('head title')
+    title.innerText = [appname, ...strs].join(' - ')
+}
 
 module.exports = {
-    Trie,
-    DefaultDict,
-    sorted,
-    reversed,
-    groupByLength,
+    applyColor,
+    changeAppUIColor,
+    makeCollapsable,
+    showWordCount,
+    updateTitle,
 }
